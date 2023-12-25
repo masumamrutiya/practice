8ddf46ccbcb5ab7a5706af276c929b59557c1e06
masumamrutiya <masumamrutiya29@gmail.com>
2023-12-25 11:50:02 +0530

    print-f

---
+++ index.c
@@ -0,0 +1,14 @@
+
+#include <stdio.h>
+
+int main()
+{
+     
+  float a;
+  printf("enter your number in float");
+  scanf("%f",&a);
+  printf("%f\n",a);
+  printf("%0.3f",a);
+  
+    return 0;
+}
